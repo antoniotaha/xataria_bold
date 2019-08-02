d81d0ab3a9efe4544c1cefbd1841e40cf8260dd9
antoniotaha <antoniotahasc@gmail.com>
2019-08-02 13:00:09 -0300

    Ajuste home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import { ThemeProvider, lightTheme } from "bold-ui";
+import { ThemeProvider, lightTheme, withStyles } from "bold-ui";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeView from "./view/HomeView";
 import NotHomeView from "./view/NotHomeView";
 import MenuBar from "./components/Menubar";
 
-function App() {
+function App(props) {
+  const { css, location, theme } = props;
+
+  const styles = {
+    container: {
+      background: theme.pallete.surface.main,
+      display: "grid",
+      gridTemplateAreas: `"header header"
+                                     "nav main"`,
+      gridTemplateColumns: "5rem 1fr"
+    },
+    header: {
+      gridArea: "header"
+    },
+    nav: {
+      gridArea: "nav"
+    },
+    main: {
+      gridArea: "main"
+    }
+  };
   return (
-    <div className="App">
-      <ThemeProvider theme={lightTheme}>
-        <Router>
+    <ThemeProvider theme={lightTheme}>
+      <Router>
+        <nav className={css(styles.nav)}>
           <MenuBar />
-          {/* <Route exact path="/" component={HomeView} />
-        <Route path="/teste" component={NotHomeView} /> */}
-        </Router>
-      </ThemeProvider>
-    </div>
+        </nav>
+        <main className={css(styles.main)}>
+          <Route exact path="/" component={NotHomeView} />
+        </main>
+      </Router>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default withStyles(App);
