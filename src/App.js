b89591d049a61d961d848dcf53668a384c7cab22
Gustavo Moser <gustavovbmoser@gmail.com>
2019-08-02 12:53:14 -0300

    top bar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeView from "./view/HomeView";
 import NotHomeView from "./view/NotHomeView";
 import MenuBar from "./components/Menubar";
+import { VTabs } from "./components/VTabs";
+import { UserBar } from "./view/user/UserBar";
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={lightTheme}>
         <Router>
+          <UserBar></UserBar>
           <MenuBar />
           {/* <Route exact path="/" component={HomeView} />
         <Route path="/teste" component={NotHomeView} /> */}
