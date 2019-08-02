7d13c5e3a161bf4360285aad70f55b6511b0c6d4
antoniotaha <antoniotahasc@gmail.com>
2019-08-02 10:21:20 -0300

    Roteamento na aplicação

---
+++ src/view/HomeView.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route } from "react-router";
+
+function HomeView() {
+  return <label>Home view carregada</label>;
+}
+
+export default HomeView;
---
+++ src/view/NotHomeView.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Route } from "react-router";
+
+function NotHomeView() {
+  return <label>Not Home view carregada</label>;
+}
+
+export default NotHomeView;
